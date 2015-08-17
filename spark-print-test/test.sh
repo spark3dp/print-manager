c81e09ca6465157b87dd81f8f4a489706f11c7d0
daviesm <mark.davies@autodesk.com>
2015-08-17 13:59:26 -0400

    Deleting unused testing Directory

--- spark-print-test/test.sh
+++
@@ -1,8 +0,0 @@
-node ../spark-print-mgr/server.js &
-print_manager_pid=$!
-sleep 3
-python ../spark-print-client/spark.py uploadDB ../spark-print-data/
-sleep 2
-jasmine-node $1
-sleep 1
-kill $print_manager_pid
